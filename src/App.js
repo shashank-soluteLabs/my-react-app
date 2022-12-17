b6e1401f1a5d44b14c3a3d5789f165f5c0a5d4bd
shashank-soluteLabs <shashank.s@solutelabs.com>
2022-12-17 21:45:09 +0530

    png and svg added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import slt from "./images/slt.png";
+import scooby from "./images/scooby.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>hello from App</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={slt} alt="slt" />
+        <img src={scooby} alt="scooby" />
       </header>
     </div>
   );
